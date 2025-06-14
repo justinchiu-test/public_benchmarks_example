d6b7551fba8164423a716500269536d09ca63ef4
Justin Chiu <justinchiu@cohere.com>
2025-06-14 02:39:55 +0000

    print scenarios

--- list_benchmarks.py
+++ list_benchmarks.py
@@ -5,3 +5,9 @@ benchmarks = client.benchmarks.list_public()
 
 for benchmark in benchmarks:
     print(benchmark.name, benchmark.id)
+    if "smith" in benchmark.name:
+        x = benchmark
+
+scenario_ids = x.scenario_ids
+scenarios = [client.scenarios.retrieve(id) for id in scenario_ids[:10]]
+
