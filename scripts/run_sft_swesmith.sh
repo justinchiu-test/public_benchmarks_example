109eae4695491964e9b477506706984ea3ce1fa4
Justin Chiu <justinchiu@cohere.com>
2025-06-15 06:23:32 +0000

    start on swesmith subset

--- scripts/run_sft_swesmith.sh
+++ scripts/run_sft_swesmith.sh
@@ -76,7 +76,7 @@ export CONFIG=/home/justinchiu_cohere_com/SWE-agent/config/default_last10swesmit
 #export CONFIG=/home/justinchiu_cohere_com/SWE-agent/config/default_lastn_oh.yaml
 
 uv run run_public_benchmark.py \
-    --benchmark-id bmd_2zmp3Mu3LhWu7yDVIfq3m \
+    --benchmark-id bmd_30CcB9EompRi7i7t7srf2 \
     --config-path $CONFIG \
     --timeout-secs 1200 \
     --concurrent_runs $CONCURRENT \
