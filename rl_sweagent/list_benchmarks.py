1da78045fe79dcb7ac2eadbd000f6f5c5575723e
Justin Chiu <justinchiu@cohere.com>
2025-06-25 22:48:10 +0000

    update benchmark list

--- rl_sweagent/list_benchmarks.py
+++ rl_sweagent/list_benchmarks.py
@@ -13,6 +13,11 @@ def main():
     scenario_ids = x.scenario_ids
     scenarios = [client.scenarios.retrieve(id) for id in scenario_ids[:10]]
 
+    # private
+    benchmarks = client.benchmarks.list()
+    for benchmark in benchmarks:
+        print(benchmark.name, benchmark.id)
+
 
 if __name__ == "__main__":
     main()
